67558c93f8ddd1fd89a3c2415f3b85f1e5b4d820
myzk-a <akitoshi19910309@gmail.com>
2022-01-01 18:07:32 +0900

    尺取り法

---
+++ 尺取り法/main.cpp
@@ -0,0 +1,35 @@
+﻿// 034 - There are few types of elements（★4）https ://atcoder.jp/contests/typical90/tasks/typical90_ah
+// 解説 https://github.com/E869120/kyopro_educational_90/blob/main/editorial/034.jpg
+
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ll N, K;
+    cin >> N >> K;
+    vector<ll> A(N);
+    for (ll i = 0; i < N; ++i) cin >> A[i];
+
+    ll cnt = 0;
+    ll right = 0;
+    map<ll, ll> m;
+    ll ans = 0;
+    for (ll left = 0; left < N; ++left)
+    {
+        while (right < N)
+        {
+            if (m[A[right]] == 0 && cnt == K) break;
+            if (m[A[right]] == 0) ++cnt;
+            ++m[A[right]];
+            ++right;
+        }
+        
+        ans = max(ans, right - left);
+        if (m[A[left]] == 1) --cnt;
+        --m[A[left]];
+    }
+    cout << ans << endl;
+    return 0;
+}
